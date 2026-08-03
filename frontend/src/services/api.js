ce33e14e6a581003b54578c27acf856f09c49e04
shivaaniganesh2307-source <shivaaniganesh2307@gmail.com>
2026-08-03 16:45:54 +0000

    changesMade

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,6 @@
 // Replace the domain below with your exact copied Port 5000 Forwarded Address from VS Code
-const API_BASE_URL = "http://127.0.0.1:5000/api";
+const API_BASE_URL = "https://opulent-space-trout-69jwgr5rr67rcx45x-5000.app.github.dev/api";
+
 
 export async function sendChatMessage(sessionId, formName, message) {
   try {
